4e88e20311f28a23535cb9c7c88aab6095ff7369
Cory Edwards <Cory Edwards@Corys-computer>
2017-09-10 12:50:21 -0400

    Finished client code for problem 1.

--- Cpsc313/src/Problems/prob1.java
+++ Cpsc313/src/Problems/prob1.java
@@ -6,6 +6,7 @@
 package Problems;
 
 import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdOut;
 
 /**
  *
@@ -17,8 +18,12 @@ public class prob1
     {
         In in = new In(args[0]);
         int n = in.readInt();
-        GroupList<Integer> groups = new GroupList();
-//        Groups groups = new Groups(n);
+        
+        //Import version
+//        GroupList<Integer> groups = new GroupList();
+
+        //Non import version
+        Groups groups = new Groups(n);
         
         String line;
         while(in.hasNextLine())
@@ -32,6 +37,18 @@ public class prob1
         }
         
         in.close();
-//        System.out.println(groups.toString());
+        
+        //Print out for Prob1Data1.txt
+        StdOut.println("1 and 2 together? " + groups.inSameGroup(1, 2));
+        StdOut.println("2 and 7 together? " + groups.inSameGroup(2, 7));
+        StdOut.println("3 and 5 together? " + groups.inSameGroup(3, 5));
+        StdOut.println("6 and 7 together? " + groups.inSameGroup(6, 7));
+        StdOut.println("1 and 8 together? " + groups.inSameGroup(1, 8));
+        StdOut.println("1 and 9 together? " + groups.inSameGroup(1, 9));
+        StdOut.println();
+        StdOut.println("Number of groups: " + groups.getNumGroups());
+        StdOut.println("Number of items: " + groups.getNumItems());
+        StdOut.println();
+        StdOut.println(groups.toString());
     }
 }
