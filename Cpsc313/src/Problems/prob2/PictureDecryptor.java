90c50a0989e232dccffd0f62c724f3107486e550
14corman <14corman@gmail.com>
2017-09-20 13:58:41 -0400

    Finished problem 2.

--- Cpsc313/src/Problems/prob2/PictureDecryptor.java
+++ Cpsc313/src/Problems/prob2/PictureDecryptor.java
@@ -19,47 +19,29 @@ public class PictureDecryptor
     {
         Picture in = new Picture(args[0]);
         Picture out = new Picture(in.width(), in.height());
-        in.show();
+//        in.show();
         
-        Stopwatch watch = new Stopwatch();
         int dim = in.width() * in.height();
         int wi = 0;
         int he = 0;
         for(int i = 0; i < dim; i++)
         {
+            Color p = in.get(wi, he);
+            Color p2 = new Color(
+                (p.getRed() & 1) == 1 ? 255 : 0,
+                (p.getGreen() & 1) == 1 ? 255 : 0, 
+                (p.getBlue() & 1) == 1 ? 255 : 0);
+            out.set(wi, he, p2);
+            
             if(wi == in.width() - 1)
             {
                 wi = 0;
                 he++;
             }
-            
-            Color p = in.get(wi, he);
-            System.out.println(p.getRed());
-            System.out.println((p.getRed() & 1) == 1 ? 255 : 0);
-            out.set(wi, he, new Color(
-                (p.getRed() & 1) == 1 ? 255 : 0,
-                (p.getGreen() & 1) == 1 ? 255 : 0, 
-                (p.getBlue() & 1) == 1 ? 255 : 0));
+            else
+                wi++;
         }
-        System.out.println("Time = " + watch.elapsedTime());
-        
-//        Stopwatch watch = new Stopwatch();
-//        for(int w = 0; w < in.width(); w++)
-//        {
-//            for(int h = 0; h < in.height(); h++)
-//            {
-//                Color p = in.get(w, h);
-//                System.out.println(p.getRed());
-//                System.out.println((p.getRed() & 1) == 1 ? 255 : 0);
-//                int r = (p.getRed() & 1) == 1 ? 255 : 0;
-//                int g = (p.getGreen() & 1) == 1 ? 255 : 0;
-//                int b = (p.getBlue() & 1) == 1 ? 255 : 0;
-//                Color converted = new Color(r, g, b);
-//                out.set(w, h, converted);
-//            }
-//        }
-//        System.out.println("Time = " + watch.elapsedTime());
-        
-        out.show();
+//        out.show();
+        out.save("Uncovered1A.png");
     }
 }
