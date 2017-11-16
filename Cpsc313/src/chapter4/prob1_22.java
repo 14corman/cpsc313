cc9e85880477b2769e7c0b3276862075f5fe9c92
Cory Edwards <Cory Edwards@Corys-computer>
2017-11-15 21:54:53 -0500

    Worked on problem 4.1.22.

--- Cpsc313/src/chapter4/prob1_22.java
+++ Cpsc313/src/chapter4/prob1_22.java
@@ -5,6 +5,10 @@
  */
 package chapter4;
 
+import edu.princeton.cs.algs4.BreadthFirstPaths;
+import edu.princeton.cs.algs4.Graph;
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.SymbolGraph;
 
 /**
@@ -12,9 +16,39 @@ import edu.princeton.cs.algs4.SymbolGraph;
  * @author Cory Edwards
  */
 public class prob1_22 {
-    public static void main(String[] args)
-    {
-        SymbolGraph sg = new SymbolGraph(args[0], args[1]);
-        
+    public static void main(String[] args) {
+        String filename  = args[0];
+        String delimiter = args[1];
+        String source    = args[2];
+
+        // StdOut.println("Source: " + source);
+
+        SymbolGraph sg = new SymbolGraph(filename, delimiter);
+        Graph G = sg.graph();
+        if (!sg.contains(source)) {
+            StdOut.println(source + " not in database.");
+            return;
+        }
+
+        int s = sg.indexOf(source);
+        BreadthFirstPaths bfs = new BreadthFirstPaths(G, s);
+
+        while (!StdIn.isEmpty()) {
+            String sink = StdIn.readLine();
+            if (sg.contains(sink)) {
+                int t = sg.indexOf(sink);
+                if (bfs.hasPathTo(t)) {
+                    for (int v : bfs.pathTo(t)) {
+                        StdOut.println("   " + sg.nameOf(v));
+                    }
+                }
+                else {
+                    StdOut.println("Not connected");
+                }
+            }
+            else {
+                StdOut.println("   Not in database.");
+            }
+        }
     }
 }
