3c86e191e5f2da7412e2869519219e8688162caa
Cory Edwards <Cory Edwards@Corys-computer>
2017-11-15 20:33:46 -0500

    Started problems 4.1.10, 4.1.16, 4.1.22.

---
+++ Cpsc313/src/chapter4/prob1_16.java
@@ -0,0 +1,55 @@
+package chapter4;
+
+import edu.princeton.cs.algs4.DepthFirstPaths;
+import edu.princeton.cs.algs4.Graph;
+import java.util.Iterator;
+
+/**
+ *
+ * @author Cory Edwards
+ * This would be GraphProperties
+ */
+public class prob1_16 {
+    private int center;
+    private int radius;
+    private int diameter;
+    public prob1_16(Graph G) throws Exception {
+        for(int i = 0; i < G.V(); i++)
+            if(G.degree(i) != G.V() - 1) throw new Exception();
+        getProperties(G, G.V());
+    }
+    private void getProperties(Graph G, int v){
+        DepthFirstPaths dfp = new DepthFirstPaths(G, 0);
+        int min = Integer.MAX_VALUE;
+        int max = 0;
+        int vertex = 0;
+        for(int i = 0; i < v; i++){
+            Iterable<Integer> iter = dfp.pathTo(i);
+            if(iter != null) {
+                Iterator<Integer> it = iter.iterator();
+                int count = 0;
+                while(it.hasNext()){
+                    it.next();
+                    count++;
+                }
+                if(min > count) {
+                    min = count;
+                    vertex = i;
+                }
+                if(max < count) max = count;
+            }
+        }
+        diameter = max;
+        radius = min;
+        center = vertex;
+    }
+    public int diameter(){
+        return diameter;
+    }
+    public int radius(){
+        return radius;
+    }
+    public int center(){
+        return center;
+    }
+}
---
+++ Cpsc313/src/chapter4/prob1_22.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package chapter4;
+
+import edu.princeton.cs.algs4.SymbolGraph;
+
+/**
+ *
+ * @author Cory Edwards
+ */
+public class prob1_22 {
+    public static void main(String[] args)
+    {
+        SymbolGraph sg = new SymbolGraph(args[0], args[1]);
+        
+    }
+}
